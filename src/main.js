8d9f74689f75c17f1d5ad30799bcf0a1c7bdfcdc
Haversters <1109143961@qq.com>
2020-05-25 23:22:55 +0800

    初始化配置

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+// 引入element
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 挂载到VUE
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
