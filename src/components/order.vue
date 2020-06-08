7d5dde04a23b6d2c31532ea774a67a44e291fff7
Haversters <1109143961@qq.com>
2020-06-08 22:31:31 +0800

    202006082231

--- src/components/order.vue
+++ src/components/order.vue
@@ -12,18 +12,18 @@
         style="width: 100%"
         :border="true"
       >
-        <el-table-column align="center" sortable prop="date" label="PO" width></el-table-column>
-        <el-table-column align="center" prop="date" label="负责人" width></el-table-column>
-        <el-table-column align="center" prop="name" label="采购价" width></el-table-column>
-        <el-table-column align="center" prop="address" label="重量"></el-table-column>
-        <el-table-column align="center" prop="address" label="海运预估利润 "></el-table-column>
-        <el-table-column align="center" prop="address" label="空运预估利润"></el-table-column>
-        <el-table-column align="center" prop="address" label="shipment ID"></el-table-column>
-        <el-table-column align="center" prop="address" label="invoice"></el-table-column>
-        <el-table-column align="center" prop="address" label="发票付款金额 "></el-table-column>
-        <el-table-column align="center" prop="address" label="是否已经付全款"></el-table-column>
-        <el-table-column align="center" prop="address" label="Amazon接收的到货数量"></el-table-column>
-        <el-table-column align="center" prop="address" label="备注"></el-table-column>
+        <el-table-column align="center" sortable prop="id" label="PO" width></el-table-column>
+        <el-table-column align="center" prop="person_charge" label="负责人" width></el-table-column>
+        <el-table-column align="center" prop="size" label="采购价" width></el-table-column>
+        <el-table-column align="center" prop="weight" label="重量"></el-table-column>
+        <el-table-column align="center" prop="ocean_profit" label="海运预估利润 "></el-table-column>
+        <el-table-column align="center" prop="air_profit" label="空运预估利润"></el-table-column>
+        <el-table-column align="center" prop="shipment_id" label="shipment ID"></el-table-column>
+        <el-table-column align="center" prop="invoice" label="invoice"></el-table-column>
+        <el-table-column align="center" prop="Invoice_payment_amount" label="发票付款金额 "></el-table-column>
+        <el-table-column align="center" prop="pay_state" label="是否已经付全款"></el-table-column>
+        <el-table-column align="center" prop="received_quantity" label="Amazon接收的到货数量"></el-table-column>
+        <el-table-column align="center" prop="AISN" label="备注"></el-table-column>
         <el-table-column align="center" prop="address" label="操作">
           <el-button size="mini" type="danger" @click="handleDelete(11, 111)">编辑</el-button>
         </el-table-column>
@@ -38,31 +38,7 @@ export default {
   data() {
     return {
       tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        }
+
       ],
       isCollapse: true, //控制侧边栏的显示
       search: "",
@@ -77,9 +53,13 @@ export default {
   mounted(){
     const _this=this;
        this.$fetch('/api/admin/index/index')
-      .then((response) => {
+      .then((e) => {
         console.log(111)
-        console.log(response)
+        console.log(e)
+        if(e.code==6){
+        _this.tableData=e.data
+        }
+
         console.log(_this.$store.state.user_data)
       })
   },
